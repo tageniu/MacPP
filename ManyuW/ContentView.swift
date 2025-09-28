edbb4e11429dbc33779fe0c6b0c3fab452293c9b
mumu <690481115@qq.com>
2025-09-28 09:31:55 +0800

    Initial Commit

---
+++ ManyuW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManyuW
+//
+//  Created by 木木 on 2025/9/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManyuW/ManyuWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyuWApp.swift
+//  ManyuW
+//
+//  Created by 木木 on 2025/9/28.
+//
+
+import SwiftUI
+
+@main
+struct ManyuWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
